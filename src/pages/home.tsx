40014e2950e36a0537374b7939bcf3153e52c21f
qiluzhang <qiluzhang@foresee.com.cn>
2020-03-12 17:55:28 +0800

    init

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './home.less';
+
+export default () => {
+  return (
+    <div>
+      <h1 className={styles.title}>welcome umi</h1>
+    </div>
+  );
+}
