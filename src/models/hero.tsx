9951b6bc05f0c816aebc2552fdeca065bdb2b990
Qilz <285486683@qq.com>
2020-03-16 11:05:46 +0800

    update‘

--- src/models/hero.tsx
+++ src/models/hero.tsx
@@ -1,23 +1,30 @@
 export default {
-    namespace: 'hero', // 默认与文件名相同 // 这个 model 的名字，必须全局唯一
-    state:{
-        text:'这是一个文件',
-        hero:'齐天大圣'
-    } ,
-    subscriptions: { // 用于订阅数据
-      setup({ dispatch, history }) {
-      },
+  namespace: 'hero', // 默认与文件名相同 // 这个 model 的名字，必须全局唯一
+  state: {
+    text: '这是一个文件',
+    hero: '齐天大圣',
+  },
+  subscriptions: {
+    // 用于订阅数据
+    setup({ dispatch, history }) {},
+  },
+  reducers: {
+    // 用于修改数据
+    update(state) {
+      return history.listen(({ pathname, query }) => {
+        if (pathname === '/hero') {
+          dispatch({
+            type: 'fetch',
+          });
+        }
+      });
     },
-    reducers: { // 用于修改数据
-      update(state) {
-        return `${state}_hero`;
-      },
-    },
-    effects: { // 用于获取数据
-      *fetch({ type, payload }, { put, call, select }) {
-      },
-    },
-  } 
+  },
+  effects: {
+    // 用于获取数据
+    *fetch({ type, payload }, { put, call, select }) {},
+  },
+};
 //   {
 //     namespace: 'example', // 这个 model 的名字，必须全局唯一
 //     state: {
